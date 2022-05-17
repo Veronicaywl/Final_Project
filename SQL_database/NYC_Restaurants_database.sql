56e58815fbe15246f97e01c4b42fafe8697f8893
jwhberrios <94324519+jwhberrios@users.noreply.github.com>
2022-05-17 00:22:55 -0400

    updated SQL files and images

--- SQL_database/NYC_Restaurants_database.sql
+++ SQL_database/NYC_Restaurants_database.sql
@@ -1,9 +1,9 @@
 --View table
-SELECT * FROM newtable4;
+SELECT * FROM newtable5;
 
 
 --InnerJoin of databases
-SELECT c."DBA", c."STREET", c."INCOME_LEVEL", s."CUISINE_DESCRIPTION", s."SCORE", s."GRADE"
+SELECT c."DBA", c."STREET", c."INCOME_LEVEL", s."Borough", s."CUISINE_DESCRIPTION", s."SCORE", s."GRADE"
 INTO newtable4
 FROM income3 c
 INNER JOIN inspection3 s
@@ -11,10 +11,10 @@ ON c."DBA" = s."DBA" and c."STREET" = s."STREET";
 
 --Count number of rows
 SELECT
-	COUNT(*)
-FROM newtable4;
+COUNT(*)
+FROM newtable5;
 
 --SELECT DISTINCT to remove duplicate rows
-SELECT DISTINCT "DBA", "STREET", "INCOME_LEVEL","CUISINE_DESCRIPTION", "SCORE", "GRADE"
+SELECT DISTINCT "DBA", "STREET", "INCOME_LEVEL", "Borough", "CUISINE_DESCRIPTION", "SCORE", "GRADE"
 INTO newtable5
 FROM newtable4;
